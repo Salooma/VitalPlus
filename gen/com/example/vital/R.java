da03eceefa50129e31caa31da959822a784831f8
Salma Hindy <salmahindy@gmail.com>
2014-02-25 14:14:00 -0500

    added textview to one of the tabs

--- gen/com/example/vital/R.java
+++ gen/com/example/vital/R.java
@@ -28,11 +28,12 @@ public final class R {
         public static final int vitals_icon=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int button1=0x7f080003;
         public static final int ibMainProfile=0x7f080002;
         public static final int ibMainVitals=0x7f080000;
-        public static final int pager=0x7f080004;
+        public static final int pager=0x7f080005;
+        public static final int text=0x7f080004;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
